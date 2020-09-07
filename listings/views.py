7e83bd00baffddb45d5671104d2d5ac97016f609
MarioJean <42340502+MarioJean@users.noreply.github.com>
2020-09-07 14:59:41 +0300

    search

--- listings/views.py
+++ listings/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Listing
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
+from .choices import price_choices, bedroom_choices, county_choices
 
 def index(request):
     listings = Listing.objects.order_by('-list_date').filter(is_published=True)
@@ -21,4 +22,11 @@ def listing(request, listing_id):
     return render(request, 'listings/listing.html', context)
 
 def search(request):
-    return render(request, 'listings/search.html')
+
+    context = { 
+        'price_choices':price_choices,
+        'bedroom_choices':bedroom_choices,
+        'county_choices':county_choices
+    }
+
+    return render(request, 'listings/search.html', context)
