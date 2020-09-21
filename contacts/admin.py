f966e3ecadfdcf65724ac6a1c14f81b21a4e3c8c
MarioJean <42340502+MarioJean@users.noreply.github.com>
2020-09-21 21:33:10 +0300

    Contact Form Submission

--- contacts/admin.py
+++ contacts/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from . models import Contact
 
-class  ContactAdmin(admin.ModelAdmin):
+class ContactAdmin(admin.ModelAdmin):
     list_display = ('id', 'name', 'listing', 'email', 'contact_date')
     list_display_links = ('id', 'name')
     search_fields = ('name', 'email', 'listing')
