74727544a0ec4a98e804ca5752195dd3642d01e5
MarioJean <42340502+MarioJean@users.noreply.github.com>
2020-09-23 13:18:38 +0300

    Dashboard

--- btre/settings.py
+++ btre/settings.py
@@ -142,3 +142,10 @@ MESSAGE_TAGS = {
     messages.ERROR: 'danger'
 }
 
+# Email Config
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'tourszahara@gmail.com'
+EMAIL_HOST_PASSWORD = 'tembeakenya2020'
+EMAIL_USE_TLS = True
